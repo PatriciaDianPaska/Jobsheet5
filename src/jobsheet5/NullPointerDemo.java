717368662d29b0c43093c308c327dc6e5422d669
ASUS <ASUS@DESKTOP-1EH2A1L>
2018-09-06 10:40:12 +0700

    jobsheet5

---
+++ src/jobsheet5/NullPointerDemo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NullPointerDemo {
+    public static void main(String [] args){
+        String name="isi nama masing";
+        System.out.println("Length of the string "+ name.length());
+    }
+}
